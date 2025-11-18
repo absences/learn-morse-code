5353ceefa95945cdff12f7f1942fe27cc98bf35e
xiewen <x826190447@126.com>
2025-11-18 11:43:11 +0800

    add greate train

--- src/contexts/wordListPickerContext.js
+++ src/contexts/wordListPickerContext.js
@@ -5,6 +5,7 @@ import common100 from '../data/common100.json'
 import boys from '../data/names_boys.json'
 import girls from '../data/names_girls.json'
 import trek from '../data/startrek.json'
+import greate from '../data/greate.json'
 
 
 const WordListPickerContext = React.createContext()
@@ -13,8 +14,6 @@ function WordListPickerContextProvider(props) {
 
     const [wordListCategory, setWordListCategory] = useState('alphabet')
     
-    const [wordListCount, setWordListCount] = useState(26)
-
     let wordList = []
 
     switch (wordListCategory) {
@@ -36,10 +35,14 @@ function WordListPickerContextProvider(props) {
         case "common100":
             wordList = common100.words
             break
+        case "greate":
+            wordList = greate.words
+            break
         default:
             wordList = alphabet.words
     }
 
+    const [wordListCount, setWordListCount] = useState(wordList.length)
     const wordListCountMax = wordList.length
 
     const metadata = {
@@ -48,7 +51,8 @@ function WordListPickerContextProvider(props) {
         'boys': {name: '男孩名字', description: '流行的男孩名字'},
         'girls': {name: '女孩名字', description: '流行的女孩名字'},
         'startrek': {name: '星际 迷航', description: '星际 迷航 宇宙'},
-        'common100': {name: '常用词', description: '100个常用词'}
+        'common100': {name: '常用词', description: '100个常用词'},
+        'greate': {name: '远大前程', description: '<远大前程>第一章'},
     }
 
     // Shuffle input array and return
